c2e35b0d06e7b156dade873fa8ad1b986b11897e
tamaki_tech <oshaburimegane@gmail.com>
2021-09-20 09:33:00 +0900

    smui適用

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,49 @@
 <script lang="ts">
-  export let name: string;
+  import Button, { Label } from "@smui/button";
+  import Slider from "@smui/slider";
+
+  let value = 0;
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
+  <h1 class="title">Svelte Pomodoro!</h1>
+  <h1>00:00</h1>
   <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
+   This is a web timer app for pomodoro techniques with Svelte.js
   </p>
+  <div>
+    <Button variant="raised">
+      <Label>Start</Label>
+    </Button>
+    <Button variant="raised">
+      <Label>Stop</Label>
+    </Button>
+  </div>
+  <br />
+  <div>
+    <h2>Work Time</h2>
+    <Slider
+      bind:value
+      min={-10}
+      max={10}
+      step={2}
+      discrete
+      input$aria-label="Discrete slider"
+    />
+  </div>
+  <div>
+    <h2>Break Time</h2>
+    <Slider
+      bind:value
+      min={-10}
+      max={10}
+      step={2}
+      discrete
+      input$aria-label="Discrete slider"
+    />
+  </div>
+
+  <pre class="status">Value: {value}</pre>
 </main>
 
 <style lang="scss">
@@ -21,23 +57,25 @@
   }
 
   h1 {
-    color: #ff3e00;
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
+    color: #ff3e00;
   }
-
   // SmartPhone
   @media screen and (max-width: 480px) {
     main {
       background-color: aqua;
+    }
+    h1 {
+      font-size: 2.5em;
     }
   }
 
   // Tablet
   @media screen and (min-width: 481px) and (max-width: 1024px) {
     main {
-      max-width: none;
+      max-width: 480px;
       background-color: bisque;
     }
   }
@@ -45,7 +83,7 @@
   // PC
   @media screen and (min-width: 1025px) {
     main {
-      max-width: none;
+      max-width: 600px;
     }
   }
 </style>
